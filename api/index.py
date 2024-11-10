16cd1be58c182b9af3f418a955225d6f7e1c82a5
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2024-11-11 02:38:22 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,16 +5,11 @@ from flask_cors import CORS
 app = flask.Flask(__name__)
 CORS(app, resources=r'/*')
 
-@app.route('/github/<path:ghpath>', methods=["GET", "POST"])
+@app.route('/hitokoto', methods=["GET"])
 def github(ghpath):
-    url = 'https://github.com/{}'.format(ghpath)
-    params = {
-        'client_id': flask.request.json['client_id'],
-        'client_secret': flask.request.json['client_secret'],
-        'code': flask.request.json['code']
-    }
+    url = 'https://v1.hitokoto.cn/'
     headers = {
         'accept': 'application/json'
     }
-    result = requests.post(url=url, params=params, headers=headers, verify=False)
+    result = requests.get(url=url, headers=headers, verify=False)
     return result.json()
